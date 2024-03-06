bc87928ba4bb1433fb8fb42903c7235c2050f824
courseKA <150334193+courseKA@users.noreply.github.com>
2024-03-06 19:29:11 +0200

    Update scrape2.py

--- scrape2.py
+++ scrape2.py
@@ -4,11 +4,15 @@ class JarspiderSpider(scrapy.Spider):
     start_urls = ["https://jarcomputers.com/Laptopi_cat_2.html?ref=c_1"]
 
     def parse(self, response):
-        products = response.css('div#products-container div ol#product_list.p1 li.sProduct.p.e-7 div.s2 ul.pprop \
-li.list_brand.brand div.brand-name a.brand-name')\
-.get().replace('<a href="', ' ').replace('" class="brand-name" style>Lenovo</a>', '')
+        products = response.css('a.plttl::text').getall()
+        
         for product in products:
             yield{
                 'name': response.css('a.plttl::text').get(),
-                'screensize': response.css('a.plttl::text').get().split()[11]
             }
+
+        for i in range(2, 6):
+            next_url = f"https://www.jarcomputers.com/laptopi-cat-2.html?ref=c_1&page={i}"
+            yield response.follow(next_url, callback=self.parse)
+
+
