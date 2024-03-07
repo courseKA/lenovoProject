0bdba6b31842db456d95324fa547b39c0363a381
courseKA <150334193+courseKA@users.noreply.github.com>
2024-03-07 08:13:36 +0200

    Update scrape2.py

--- scrape2.py
+++ scrape2.py
@@ -1,18 +1,1 @@
-class JarspiderSpider(scrapy.Spider):
-    name = "jarspider"
-    allowed_domains = ["jarcomputers.com"]
-    start_urls = ["https://jarcomputers.com/Laptopi_cat_2.html?ref=c_1"]
-
-    def parse(self, response):
-        products = response.css('a.plttl::text').getall()
-        
-        for product in products:
-            yield{
-                'name': response.css('a.plttl::text').get(),
-            }
-
-        for i in range(2, 6):
-            next_url = f"https://www.jarcomputers.com/laptopi-cat-2.html?ref=c_1&page={i}"
-            yield response.follow(next_url, callback=self.parse)
-
 
